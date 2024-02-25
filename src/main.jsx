a87d5d2677c19f181f98d8504d6eded4eec79a60
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-02-25 16:44:08 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Alternative from './Alternative';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Alternative />
+  </React.StrictMode>
+);
